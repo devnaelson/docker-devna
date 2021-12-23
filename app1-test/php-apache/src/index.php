7c1b5d5c5a1cec3dc3aa9e0f35504d5cb2356d2a
= <naelson.g.saraiva@gmail.com>
2021-12-23 15:07:47 -0400

    Up

--- app1-test/php-apache/src/index.php
+++ app1-test/php-apache/src/index.php
@@ -1,3 +1,16 @@
-<?php  
+<?php
 
-echo "hello";
+echo 'Versão Atual do PHP: ' . phpversion();
+
+$servername = "testComposer";
+$username = "root";
+$password = "admin123.";
+
+try {
+    $run_federal = new PDO("mysql:host=localhost;dbname=" . $servername . ";charset=utf8", $username, $password);
+    $run_federal->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "true";
+} catch (PDOException $e) {
+    error_reporting(0);
+    echo "Erro de conexão main.php, Linha:" . __LINE__;
+}
