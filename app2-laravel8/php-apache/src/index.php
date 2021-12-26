b17e00244bc99f1ef0f4f6866c4aefd39fd21081
= <naelson.g.saraiva@gmail.com>
2021-12-26 06:20:57 -0400

    Up

---
+++ app2-laravel8/php-apache/src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Versão Atual do PHP: ' . phpversion();
+//echo 'Versão Atual do PHP: ' . phpinfo();
+
+$hostname_d = "host.docker.internal";
+$username_d = "usernaelson";
+$password_d = "admin1234.";
+$database_d = "test";
+
+try {
+    $conn = new PDO("mysql:host=$hostname_d;port=3307;dbname=" . $database_d . ";charset=utf8", $username_d, $password_d);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
---
+++ app2-laravel8/readme.md
@@ -0,0 +1,10 @@
+##### Goal (Last PHP,MYSQL) in the simples why without use images of trird
+- Step one build i simples docker file
+- Step two build composer file and send manualy settings into from docker container
+- Step Three Connect Mysql Container with PHP
+- All Working 
+- Step Four rebuild commands more complex eliminated the simples and manual way to use, step by one
+
+
+
+https://dev.to/truthseekers/setup-a-basic-local-php-development-environment-in-docker-kod
